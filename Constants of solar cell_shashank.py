cce943e23cd7e82bac38e1ad9d91dfb71157001a
shashank0824 <singhshashank0824@gmail.com>
2024-08-24 00:22:29 +0530

    Add files via upload

---
+++ Constants of solar cell_shashank.py
@@ -0,0 +1,19 @@
+'''Physical Constants'''
+theta_i = 0                        #Angle of incidence
+h = 6.63e-34                       #Planck's const  (J s)
+hbar = 1.05e-34                    #Planck's const  (J s)
+c = 3e8                            #Speed of light in a vac  (m /s)
+q = 1.60218e-19                    #Unit charge (C)
+n_air = 1                          #Refractive index of air
+T = 300                            #Temp (K)
+k = 1.38064852e-23                 #Boltzmann const ( m^2 kg s^-2 K^-1)
+A = 1e-4                           #Cross sectional area of solar cell (m^2)
+e = 8.854187817620e-12             #Vacuum permittivity (F⋅m−1)
+
+
+
+
+
+
+
+
